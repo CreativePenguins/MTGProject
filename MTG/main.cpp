a54e964270f4da282aa8cecefdae43cf48eefea6
Tyler Pearson <tpearson1994@gmail.com>
2016-02-12 23:23:33 -0600

    Initial Commit

---
+++ MTG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MTG
+//
+//  Created by Tyler Pearson on 2/12/16.
+//  Copyright © 2016 JandT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
