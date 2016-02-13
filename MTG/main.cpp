9400609b5c6d4b11899470fb11f67dc9a279ccbe
Julien Vachon <electechvach@gmail.com>
2016-02-13 03:37:23 -0500

    Hello World!

--- MTG/main.cpp
+++ MTG/main.cpp
@@ -2,17 +2,20 @@
 //  main.cpp
 //  MTG
 //
-//  Created by Tyler Pearson on 2/12/16.
+//  Created by Tyler Pearson & Julien Vachon on 2/12/16.
 //  Copyright © 2016 JandT. All rights reserved.
 //
 
 #include <iostream>
 
-int main() {
+void main() {
     std::cout << "Please enter a number...";
-    int x;
+    double x;
+	double y=774382;
     std::cin >> x;
     for (int i = 0; i!=x; i++) {
         std::cout << i << ": Hello\n";
+		y *= i;
+		std::cout << y;
     }
 }
