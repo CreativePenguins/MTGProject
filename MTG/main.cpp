85032b7224e050ebb6883658e454fa2274515acd
Tyler Pearson <tpearson1994@gmail.com>
2016-02-13 01:54:31 -0600

    files...

--- MTG/main.cpp
+++ MTG/main.cpp
@@ -8,8 +8,11 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    std::cout << "Please enter a number...";
+    int x;
+    std::cin >> x;
+    for (int i = 0; i!=x; i++) {
+        std::cout << i << ": Hello\n";
+    }
 }
