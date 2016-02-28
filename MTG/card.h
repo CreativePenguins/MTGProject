eba90217039760b123a5b06ccdb92b850317f468
Julien Vachon <electechvach@gmail.com>
2016-02-27 21:54:34 -0500

    Settting up card class boogaloo

--- MTG/card.h
+++ MTG/card.h
@@ -1,15 +1,45 @@
 #pragma once
+#ifndef player_h
+#define player_h
+#include <iostream>
+#include <string>
+#include <vector>
+
 class card
 {
 private:
 	
 	bool tap;
 	bool flip;
-	bool 
 
+	int power;
+	int toughness;
+
+	std::vector <std::string> colour;
+	std::vector <std::string> Mana;
+	std::vector <std::string> type;
+	std::vector <std::string> counter;
 
 public:
+	// Constructors
 	card();
+	
+	bool isTap();
+	bool isFlip();
+
+	int getPower();
+	int getToughness();
+
+	void tapCard();
+	void flipCard();
+	void setPower(int x);
+	void setToughness(int x);
+	void addColour(std::string x);
+	void addMana();
+
+
 
 };
+
+#endif
 
