dd6f49abef49d6147fef77b30b29ff560a65c742
Tyler Pearson <tpearson1994@gmail.com>
2016-03-08 23:15:43 -0600

    Added counter class and functions

--- MTG/counter.cpp
+++ MTG/counter.cpp
@@ -7,3 +7,48 @@
 //
 
 #include "counter.hpp"
+#include <string>
+#include <iostream>
+
+using namespace::std;
+
+/*****************************
+ Constructors
+ *****************************/
+Counter::Counter(){
+    effectNum = NULL;
+    effectType = "NULL";
+    desc = "NULL";
+}
+Counter::Counter(int num, string type, string desc){
+    effectNum = num;
+    effectType = type;
+    this->desc = desc;
+}
+
+/*****************************
+ Accessors
+ *****************************/
+
+int Counter::getEffNum(){
+    return effectNum;
+}
+string Counter::getEffType(){
+    return effectType;
+}
+string Counter::getDesc(){
+    return desc;
+}
+
+/*****************************
+ Mutators
+ *****************************/
+void Counter::setEffNum(int x){
+    effectNum = x;
+}
+void Counter::setEffType(string x){
+    effectType = x;
+}
+void Counter::setDesc(string x){
+    desc = x;
+}
--- MTG/counter.hpp
+++ MTG/counter.hpp
@@ -9,6 +9,45 @@
 #ifndef counter_hpp
 #define counter_hpp
 
-#include <stdio.h>
+#include <iostream>
+#include <string>
+
+/*
+ * Power counter, touhgness, nondescript -> player added action.
+ */
+
+using namespace::std;
+
+class Counter {
+private:
+    
+    int effectNum;      // This will be the number that actually affects the tough/power.
+    string effectType;  // This will be the type: i.e. Toughness, Power, Nondescript.
+    string desc;        // Obviously the description.
+    
+public:
+    
+    /*****************************
+     Constructors
+     *****************************/
+    Counter();
+    Counter(int num, string type, string desc);
+    
+    /*****************************
+     Accessors
+     *****************************/
+    int getEffNum();
+    string getEffType();
+    string getDesc();
+    
+    /*****************************
+     Mutators
+     *****************************/
+    void setEffNum(int x);
+    void setEffType(string x);
+    void setDesc(string x);
+    
+    
+};
 
 #endif /* counter_hpp */
